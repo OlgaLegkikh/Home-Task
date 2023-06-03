f75055eae696585b9c94b361ea87cf8898fbc704
OlgaLegkikh <23OShe@gmail.com>
2023-06-03 23:18:47 +0300

    Task 1 solution

---
+++ Task_1/Task_1/Task_1.cs
@@ -0,0 +1,22 @@
+﻿/*Task 1 Descreption С помощью цикла со счетчиком вывести на экран 
+ * в одну строку все двузначные числа, кратные 5.
+*/
+using System;
+
+namespace Task_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 10; i < 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
