bd3a6157f513053d58b812843c3420edfed4501c
OlgaLegkikh <23OShe@gmail.com>
2023-06-03 23:27:10 +0300

    Task 2 solution

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*2) С помощью цикла с постусловием(do...while) вывести на экран 
+ * в столбик последовательность чисел : -20, -40, ...,-100.
+ */
+
+using System;
+
+namespace Task_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = -20;
+            do
+            {
+                Console.WriteLine(i);
+                i -= 20;
+
+            } while (i >= -100);
+        }
+    }
+}
