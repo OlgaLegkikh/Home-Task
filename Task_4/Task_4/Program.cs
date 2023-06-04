69fe036aaecdd8c3caeba8d4331586e22a498f40
OlgaLegkikh <23OShe@gmail.com>
2023-06-04 21:48:50 +0300

    Task 4 solution

---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Создать массив элементов произвольного типа. 
+ * С помощью циклов перебрать все элементы этого массива и вывести их в консоль.
+*/
+
+using System;
+
+namespace Task_4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            object[] arr1 = { 15, "Hello", 15.64, "Disco" };
+
+            foreach (object arr in arr1)
+                {
+                Console.WriteLine(arr);
+            }
+            
+        }
+    }
+}
