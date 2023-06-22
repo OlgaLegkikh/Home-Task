2000bd3d946549eca6db2d096f78940a26375d36
OlgaLegkikh <23OShe@gmail.com>
2023-06-23 00:49:35 +0300

    Task 3-2 Done

--- Task_3_2/Task_3_2/Program.cs
+++ Task_3_2/Task_3_2/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * - Создать двумерный массив 3х3. Значения элементов массива инициализировать при объявлении массива. 
+ * - Вывести на экран значения максимального элемента каждого ряда
+*/
+
+using System;
+using System.Linq;
 
 namespace Task_3_2
 {
@@ -6,7 +12,13 @@ namespace Task_3_2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] arrayOfArrays = {
+                    new int[3]{17,22,30},
+                    new int[3]{6,70,8}
+                    };
+           
+         Console.WriteLine($"Максимальное значение первого ряда {arrayOfArrays[0].Max()}");
+         Console.WriteLine($"Максимальное значение второго ряда {arrayOfArrays[1].Max()}");
         }
     }
 }
