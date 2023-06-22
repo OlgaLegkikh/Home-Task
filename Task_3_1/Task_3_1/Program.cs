ef0a912fbc119fb06e3bcd4cd934db9d9828e571
OlgaLegkikh <23OShe@gmail.com>
2023-06-22 23:03:01 +0300

    first version of task 3 - 1

---
+++ Task_3_1/Task_3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*- Объявить и проинициализировать массив int[] на 6 элементов.
+- Ввести значения элементов массива из консоли.
+- Вывести введенные отсортированные по убыванию элементы массива на экран.
+*/
+
+using System;
+
+namespace Task_3_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] userNumbers = new int[6];
+            Console.WriteLine("Введите 6 чисел через пробел");
+            string stringOfNumbers = Console.ReadLine();
+            string[] numbers = stringOfNumbers.Split(' ');
+            int i = 0;
+            foreach (string number in numbers)
+            {
+                
+                int result = Convert.ToInt32(number);
+                userNumbers[i] = result;
+                Console.WriteLine(userNumbers);
+                i += 1;
+
+            }
+            
+        }
+    }
+}
