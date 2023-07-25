643a8d39ca3efcc73c1e0fcb19100faed8f5b850
OlgaLegkikh <23OShe@gmail.com>
2023-07-25 23:16:28 +0300

    Optional Task 1 created

---
+++ Optional_Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Optional_Task_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
