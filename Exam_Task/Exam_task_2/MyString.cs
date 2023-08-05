cceb8a8e1b5d7cd17b5ebee8e804d88adbaf090d
OlgaLegkikh <23OShe@gmail.com>
2023-08-05 22:34:17 +0300

    Task 2 done

---
+++ Exam_Task/Exam_task_2/MyString.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Exam_task_2
+{
+	public class MyString
+	{
+		private string? _myString;
+
+
+		public MyString()
+		{
+			Console.WriteLine("Введите строку");
+			_myString = Console.ReadLine();
+            Console.WriteLine(_myString);
+        }
+
+		public MyString(int i)
+		{
+            Console.WriteLine("Введите строку");
+            _myString = Console.ReadLine();
+			try
+			{
+				if (i % 2 == 0)
+				{
+					_myString.ToUpper();
+				}
+				else
+				{
+					_myString.ToLower();
+				}
+			}
+			catch (NullReferenceException)
+			{
+
+				Console.WriteLine("Строка пуста");
+			}
+
+		}
+
+		public string Reverse()
+		{
+			try
+			{
+				char[] arr = this._myString.ToCharArray();
+				Array.Reverse(arr);
+				return new string(arr);
+
+			}
+			catch (NullReferenceException)
+			{
+				return "Строка пуста";
+			}
+        }
+	}
+}
+
--- Exam_Task/Exam_task_2/Program.cs
+++ Exam_Task/Exam_task_2/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Exam_task_2;
+
+MyString str1 = new MyString();
+MyString str2 = new MyString(1);
+MyString str3 = new MyString(10);
+Console.WriteLine(str1.Reverse());
+Console.WriteLine(str2.Reverse());
+Console.WriteLine(str3.Reverse());
+
 
