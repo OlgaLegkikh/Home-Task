0f7fdbbfa4bda5950f82cc6a684f70ba42f059cc
OlgaLegkikh <23OShe@gmail.com>
2023-08-05 23:18:30 +0300

    Task 3 Done

---
+++ Exam_Task/Exam_Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime date = new DateTime();
+date = DateTime.Now;
+Console.WriteLine(date.DaysFromWorldBegan());
+
+public static class DateTimeExtension
+{
+    public static string DaysFromWorldBegan(this DateTime dt)
+    {
+        return $"День ‘{dt.Day}’ месяца ‘{dt.Month}’ года ‘{ dt.Year+ 5501}’ от сотворения мира.";
+    }
+}
