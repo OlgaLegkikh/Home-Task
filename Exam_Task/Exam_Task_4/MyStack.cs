3405d64ad4747033e15abb840c907e5924217d0b
OlgaLegkikh <23OShe@gmail.com>
2023-08-06 15:40:09 +0300

    Task 4 done

---
+++ Exam_Task/Exam_Task_4/MyStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace Exam_Task_4
+{
+	public class MyStack
+	{
+		private int[] _myStack;
+		private int _stackSize;
+
+		public MyStack(int stackSize)
+		{
+			_stackSize = stackSize;
+			_myStack = new int[stackSize];
+			
+		}
+
+		public int Pop()
+		{
+			int firstElement = _myStack[0];
+            for (int i = 1; i < _myStack.Length; i++)
+            {
+                _myStack[i - 1] = _myStack[i];
+            }
+            return firstElement;
+
+		}
+
+		public void Push(int newValue)
+		{
+			var newStack = new int[] { newValue };
+            var z = new int[newStack.Length + _myStack.Length];
+			newStack.CopyTo(z, 0);
+			_myStack.CopyTo(z, newStack.Length);
+			_myStack = z;
+			Array.Resize(ref _myStack,_stackSize);
+
+        }
+
+		public void StackOutput()
+		{
+			foreach (int element in _myStack)
+			{
+				Console.Write($"{element}");
+				Console.Write(Environment.NewLine);
+			}
+		}
+	}
+}
+
---
+++ Exam_Task/Exam_Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exam_Task_4;
+
+MyStack stack = new MyStack(5);
+stack.Push(1);
+stack.Push(2);
+stack.StackOutput();
+Console.WriteLine(stack.Pop());
+stack.Push(3);
+stack.Push(4);
+stack.StackOutput();
+ 
+
